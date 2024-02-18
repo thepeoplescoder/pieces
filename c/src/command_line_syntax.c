8e994d9af49a6dd88a12dce46ea7782f5b8347c6
John Doe <thepeoplescoder@gmail.com>
2024-02-17 17:08:15 -0800

    command_line_syntax.c now follows changes in the *_ARGS arrays thanks to __is_arg_helper_macro.

--- c/src/command_line_syntax.c
+++ c/src/command_line_syntax.c
@@ -18,6 +18,20 @@ const char * const command_line_syntax___INTERLEAVED_ARGS[2] = {"--interleaved",
 #define is_split_arg command_line_syntax___is_split_arg
 #define is_merge_arg command_line_syntax___is_merge_arg
 
+#define static_array_length(a) (sizeof(a) / sizeof((a)[0]))
+
+#define __is_arg_helper_macro(x) \
+    const char * const *ptr = (x);                    \
+    int index, len          = static_array_length(x); \
+    for (index = 0; index < len; index++)             \
+    {                                                 \
+        if (!__strcmp(arg, ptr[index]))               \
+        {                                             \
+            return true;                              \
+        }                                             \
+    }                                                 \
+    return false
+
 bool command_line_syntax___is_first_task(const char * const arg)
 {
     return is_help_arg(arg) || is_split_arg(arg) || is_merge_arg(arg);
@@ -25,20 +39,20 @@ bool command_line_syntax___is_first_task(const char * const arg)
 
 bool command_line_syntax___is_help_arg(const char * const arg)
 {
-    return !__strcmp(arg, HELP_ARGS[0])  || !__strcmp(arg, HELP_ARGS[1]);
+    __is_arg_helper_macro(HELP_ARGS);
 }
 
 bool command_line_syntax___is_split_arg(const char * const arg)
 {
-    return !__strcmp(arg, SPLIT_ARGS[0]) || !__strcmp(arg, SPLIT_ARGS[1]);
+    __is_arg_helper_macro(SPLIT_ARGS);
 }
 
 bool command_line_syntax___is_merge_arg(const char * const arg)
 {
-    return !__strcmp(arg, MERGE_ARGS[0]) || !__strcmp(arg, MERGE_ARGS[1]);
+    __is_arg_helper_macro(MERGE_ARGS);
 }
 
 bool command_line_syntax___is_interleaved_arg(const char * const arg)
 {
-    return !__strcmp(arg, INTERLEAVED_ARGS[0]) || !__strcmp(arg, INTERLEAVED_ARGS[1]);
+    __is_arg_helper_macro(INTERLEAVED_ARGS);
 }
